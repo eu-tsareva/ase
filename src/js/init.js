a1b8986792db9846a578837c153ca625e4ebb854
eu-tsareva <eu.tsareva@gmail.com>
2017-04-13 12:36:37 +0300

    add header styles

--- src/js/init.js
+++ src/js/init.js
@@ -4,22 +4,22 @@
     $(".dropdown-button").dropdown();
 
     //scroll
-    var $nav = $('.page-nav'),
-        $navInner = $nav.children('.nav-wrapper'),
-        $logo = $navInner.find('.brand-logo img');
-
-    $(window).on('scroll', function(e){
-      var offset = $(window).scrollTop();
-      if(offset) {
-        $navInner.addClass('container');
-        $logo.addClass('small');
-        $nav.css({"background-color": "rgba(255, 255, 255, " + offset/100 + ")"});
-      }
-      else {
-        $navInner.removeClass('container');
-        $logo.removeClass('small');
-      }
-    });
+    // var $nav = $('.page-nav'),
+    //     $navInner = $nav.children('.nav-wrapper'),
+    //     $logo = $navInner.find('.brand-logo img');
+    //
+    // $(window).on('scroll', function(e){
+    //   var offset = $(window).scrollTop();
+    //   if(offset) {
+    //     $navInner.addClass('container');
+    //     $logo.addClass('small');
+    //     $nav.css({"background-color": "rgba(255, 255, 255, " + offset/100 + ")"});
+    //   }
+    //   else {
+    //     $navInner.removeClass('container');
+    //     $logo.removeClass('small');
+    //   }
+    // });
 
     //show map
     $('.show-map').on('click', function(e) {
